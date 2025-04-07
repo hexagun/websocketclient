bbcd9cb2ce0203e63bc80c8bed70ca12e68952de
Mathieu Moretti <mormmorm@hotmail.com>
2025-04-06 23:36:37 -0400

    Adding Game state

--- go.mod
+++ go.mod
@@ -4,4 +4,4 @@ go 1.22.5
 
 require github.com/gorilla/websocket v1.5.3
 
-require github.com/hexagun/common v0.0.5
+require github.com/hexagun/common v0.0.9
--- main.go
+++ main.go
@@ -2,21 +2,130 @@ package main
 
 import (
 	"bufio"
+	"crypto/rand"
+	"encoding/json"
 	"fmt"
 	"log"
+	"math/big"
 	"net/url"
 	"os"
+	"strconv"
 	"strings"
-	"time"
 
 	"github.com/gorilla/websocket"
 
 	"github.com/hexagun/common"
 )
 
+type Player struct {
+	id    int
+	name  string
+	token string
+}
+
+type GameState struct {
+	// state             State
+	board             [3][3]string
+	players           [2]Player
+	activePlayerIndex int
+	winner            string
+	draw              bool
+}
+
+type Game struct {
+	state         GameState
+	actionChannel chan common.Action
+	//pool          *websocket.Pool // Seems wrong
+}
+
+func NewGame() *Game {
+	return &Game{
+		state: GameState{
+			//state: InitialState,
+		},
+		actionChannel: make(chan common.Action),
+	}
+}
+
+func (g *Game) Dispatch(action common.Action) {
+	g.actionChannel <- action
+}
+
+func joinReducer(state *GameState, action common.Action) {
+	header := action.GetHeader()
+
+	if state.players[0].name == "" {
+		state.players[0].name = fmt.Sprintf("%d", header.PlayerId)
+		// 	state.PlayerXReady = true
+		//state.players[0].token = "x"
+
+		// tr := rules[state.state][0] // Join
+		// state.state = tr.State
+
+		fmt.Printf("Player 1 (%s) has connected.\n", state.players[0].name)
+	} else if state.players[1].name == "" {
+		state.players[1].name = fmt.Sprintf("%d", header.PlayerId)
+		//state.players[0].token = "o"
+		// 	state.PlayerO = player
+		// 	state.PlayerOReady = true
+		fmt.Printf("Player 2 (%s) has connected.\n", state.players[1].name)
+		// tr := rules[state.state][0] // Join
+		// state.state = tr.State
+	} else {
+		fmt.Printf("Player %s cannot join, both slots are filled.\n", header.PlayerId)
+	}
+}
+
+func (g *Game) Draw() {
+	for _, p := range g.state.players {
+		fmt.Println("Player %s", p.name)
+	}
+	board := g.state.board
+	for _, i := range []int{0, 1, 2} {
+		fmt.Println("Board [%s,%s,%s]", board[i][0], board[i][1], board[i][2])
+	}
+}
+
+func updateReducer(state *GameState, action common.Action) {
+
+	//header := action.GetHeader()
+	payload := action.GetPayload().(common.GameStateUpdatePayload)
+	state.board = payload.Board
+	state.activePlayerIndex, _ = strconv.Atoi(payload.NextTurn)
+	state.winner = payload.Winner
+}
+
+func (g *Game) GameLoop() {
+	for {
+		select {
+		case action := <-g.actionChannel:
+
+			header := action.GetHeader()
+			//payload := action.GetPayload()
+
+			switch header.Type {
+			case common.GameStateUpdate:
+				updateReducer(&g.state, action)
+			case common.Join:
+				joinReducer(&g.state, action)
+			case common.Start:
+				//startReducer(&g.state, action)
+			default:
+			}
+
+			g.Draw()
+		}
+	}
+}
+
+func GenerateCustomID() int {
+	randomNumber, _ := rand.Int(rand.Reader, big.NewInt(1000000))
+	return int(randomNumber.Int64())
+}
+
 var addr = "localhost:8100"
 var gameId = 111
-var playerId = 0
+var playerId = GenerateCustomID()
 
 type WebSocketClient struct {
 	conn *websocket.Conn
@@ -25,6 +134,11 @@ type WebSocketClient struct {
 func (wsc *WebSocketClient) Connect(addr string) {
 	if wsc.conn == nil {
 		u := url.URL{Scheme: "ws", Host: addr, Path: "/ws"}
+
+		q := u.Query()
+		q.Set("id", strconv.Itoa(playerId))
+		u.RawQuery = q.Encode()
+
 		fmt.Println(u)
 		c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
 		if err != nil {
@@ -48,32 +162,91 @@ func (wsc *WebSocketClient) Close() {
 	}
 }
 
+type GameMessageDecoder struct {
+}
+
+func (g GameMessageDecoder) Decode(msg *common.IncomingMessage) common.Action {
+	var action common.Action
+
+	if msg.GameID != "" {
+		id, err := strconv.Atoi(msg.GameID)
+		if err != nil {
+			// ... handle error
+			panic(err)
+		}
+		gameId = id
+	}
+
+	if msg.PlayerID != "" {
+		id, err := strconv.Atoi(msg.PlayerID)
+		if err != nil {
+			// ... handle error
+			panic(err)
+		}
+		playerId = id
+	}
+
+	switch msg.Type {
+	case "gamestateupdate":
+		var updatePayload struct {
+			Board    [3][3]string `json:"board"`
+			NextTurn string       `json:"nextturn"`
+			Winner   string       `json:"winner"`
+		}
+		if err := json.Unmarshal(msg.Payload, &updatePayload); err == nil {
+			//fmt.Printf("Player made a move at row %d, col %d\n", movePayload.Row, movePayload.Col)
+			action = common.NewGameStateUpdateAction(gameId, playerId, common.GameStateUpdatePayload{
+				Board:    updatePayload.Board,
+				NextTurn: updatePayload.NextTurn,
+				Winner:   updatePayload.Winner,
+			})
+		}
+	//case "gameover":
+	//case "error":
+	case "start":
+		action = common.NewStartAction(gameId, playerId)
+	case "join":
+		action = common.NewJoinAction(gameId, playerId)
+	// case "leave":
+	// 	action = common.NewLeaveAction(gameId, playerId)
+	case "move":
+		var movePayload struct {
+			Row int `json:"row"`
+			Col int `json:"col"`
+		}
+		if err := json.Unmarshal(msg.Payload, &movePayload); err == nil {
+			fmt.Printf("Player made a move at row %d, col %d\n", movePayload.Row, movePayload.Col)
+			action = common.NewPlayerMoveAction(gameId, playerId, common.PlayerMovePayload{
+				Row: movePayload.Row,
+				Col: movePayload.Col,
+			})
+		}
+
+	default:
+		fmt.Println("Not Decoding stuff")
+	}
+
+	return action
+	//game.Dispatch(action)
+}
+
 func main() {
 
 	scanner := bufio.NewScanner(os.Stdin)
+
+	game := NewGame()
+	go game.GameLoop()
+
 	client := &WebSocketClient{}
-
-	//done := make(chan struct{})
-
-	// go func() {
-	// 	defer close(done)
-	// 	for {
-	// 		_, message, err := c.ReadMessage()
-	// 		if err != nil {
-	// 			log.Println("read:", err)
-	// 			return
-	// 		}
-	// 		log.Printf("recv: %s", message)
-	// 	}
-	// }()
 
 	for {
 		fmt.Print(">: ")
 		scanner.Scan()
-		input := strings.TrimSpace(scanner.Text())
-		fmt.Println(input)
+		inputText := strings.TrimSpace(scanner.Text())
+		input := strings.Split(inputText, " ")
+		fmt.Println()
 
-		switch input {
+		switch input[0] {
 		case "connect":
 			if !client.IsConnected() {
 				client.Connect(addr)
@@ -87,69 +260,66 @@ func main() {
 							log.Println("read:", err)
 							return
 						}
+
+						var msg common.IncomingMessage
+
+						if err := json.Unmarshal(message, &msg); err != nil {
+							log.Println("JSON Unmarshal error:", err)
+							continue
+						}
+
+						fmt.Printf("Message Decoded: %+v\n", msg)
+						decoder := GameMessageDecoder{}
+						action := decoder.Decode(&msg)
+						game.Dispatch(action)
 						log.Printf("recv: %s", message)
 					}
 				}()
 			}
-		case "join":
-			if client.IsConnected() {
-				joinAction := common.NewJoinAction(gameId, playerId)
-				joinMessage := common.NewOutgoingMessage(joinAction.Type, joinAction.GameId, joinAction.PlayerId, nil)
-				client.conn.WriteJSON(joinMessage)
-			}
-		case "start":
-			if client.IsConnected() {
-				startAction := common.NewStartAction(gameId, playerId)
-				startMessage := common.NewOutgoingMessage(startAction.Type, startAction.GameId, startAction.PlayerId, nil)
-				client.conn.WriteJSON(startMessage)
-			}
 		case "play":
 			if client.IsConnected() {
-				playAction := common.NewPlayerMoveAction(gameId, playerId,
-					common.PlayerMovePayload{
-						Row: 1,
-						Col: 2,
-					},
-				)
-				playMessage := common.NewOutgoingMessage(playAction.Type, playAction.GameId, playAction.PlayerId, playAction.Payload)
-				client.conn.WriteJSON(playMessage)
+				if len(input) > 2 {
+					row, errRow := strconv.Atoi(input[1])
+					col, errCol := strconv.Atoi(input[2])
+					if errRow != nil || errCol != nil {
+						log.Println("Missing row or col")
+						break
+					}
+
+					playAction := common.NewPlayerMoveAction(gameId, playerId,
+						common.PlayerMovePayload{
+							Row: row,
+							Col: col,
+						},
+					)
+					header := playAction.GetHeader()
+					payload := playAction.GetPayload()
+					playMessage := common.NewOutgoingMessage(header.Type, header.GameId, header.PlayerId, payload)
+					client.conn.WriteJSON(playMessage)
+				}
+
 			}
 		case "update":
 			if client.IsConnected() {
-				updateAction := common.NewGameStateUpdateAction(gameId, playerId, common.GameStateUpdatePayload{
-					//TODO fill board array
-					Board:    [3][3]string{{"X", "O", "X"}, {"O", "X", "O"}, {"O", "X", "O"}},
-					NextTurn: "X",
-					Winner:   "",
-				})
-				updateMessage := common.NewOutgoingMessage(updateAction.Type, updateAction.GameId, updateAction.PlayerId, updateAction.Payload)
-				client.conn.WriteJSON(updateMessage)
-			}
-		case "gameover":
-			if client.IsConnected() {
-				gameoverAction := common.NewGameOverAction(gameId, common.GameOverPayload{
-					Winner: "Player1",
-					Board:  [3][3]string{{"X", "O", "X"}, {"O", "X", "O"}, {"O", "X", "O"}},
-				})
-				gameoverMessage := common.NewOutgoingMessage(gameoverAction.Type, gameoverAction.GameId, 0, gameoverAction.Payload)
-				client.conn.WriteJSON(gameoverMessage)
+				game.Draw()
 			}
 		case "error":
 			if client.IsConnected() {
-				errorAction := common.NewErrorAction("message", common.ErrorPayload{
-					Reason: "Failure",
-				})
-				errorMessage := common.NewOutgoingMessage(errorAction.Type, 0, 0, errorAction.Payload)
+				errorAction := common.NewErrorAction("message", "Failure")
+				// ,common.ErrorPayload{
+				// 	Reason: "Failure",
+				// })
+				header := errorAction.GetHeader()
+				payload := errorAction.GetPayload()
+				errorMessage := common.NewOutgoingMessage(header.Type, 0, 0, payload)
 				client.conn.WriteJSON(errorMessage)
 			}
-		case "time":
-			fmt.Println("Current time:", time.Now().Format(time.RFC1123))
 		case "exit":
 			fmt.Println("Exiting program. Goodbye!")
 			return
 		case "help":
 			fmt.Println("Available commands:")
-			fmt.Println("  hello  - Prints a greeting")
+			fmt.Println("  play  - Prints a greeting")
 			fmt.Println("  time   - Shows the current time")
 			fmt.Println("  exit   - Exits the program")
 			fmt.Println("  help   - Displays this help message")
